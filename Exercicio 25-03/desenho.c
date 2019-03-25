808c01e3f39534edd471be981b6f182c751f37ca
Cleanderson Lins <cleandersonlins@gmail.com>
2019-03-24 22:37:31 -0300

    Major updates

--- Exercicio 25-03/desenho.c
+++ Exercicio 25-03/desenho.c
@@ -4,6 +4,9 @@
 void SOMETHING();
 
 int angle = 10;
+int run = 0;
+
+void arrowsAction(int key, int x, int y);
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +14,8 @@ int main(int argc, char *argv[])
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(500, 500);
     glutCreateWindow("Cleanderson Lins");
+
+    glutSpecialFunc(arrowsAction);
 
     glutDisplayFunc(SOMETHING);
     glutMainLoop();
@@ -27,20 +32,31 @@ void SOMETHING(){
 
     glMatrixMode(GL_MODELVIEW);
     //glLoadIdentity();
-
     glColor3d(1,1,1);
 
+    if(run != 0){
+        glPopMatrix();
+        glLoadIdentity();
+    }
+
     glRotated(angle, 0, 0, 1);
-
     glBegin(GL_POLYGON);
-
         glVertex2i(10,70); //left top
         glVertex2i(10,10); //left bottom
         glVertex2i(70,10); //right bottom
         glVertex2i(70,70); //right top
-
     glEnd();
 
-    glFlush();
+    glPushMatrix();
+    run++;
 
+    glFlush();
+}
+
+void arrowsAction(int key, int x, int y){
+    switch(key){
+        case GLUT_KEY_UP:
+            glutPostRedisplay();
+            break;
+    }
 }
