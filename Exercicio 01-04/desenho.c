6eff69f556275390c2f01344b0f91201aa8e11c2
Cleanderson Lins <cleandersonlins@gmail.com>
2019-03-30 17:47:16 -0300

    Added axis for better understanding of movements.

--- Exercicio 01-04/desenho.c
+++ Exercicio 01-04/desenho.c
@@ -2,11 +2,31 @@
 #include <stdio.h>
 
 int firstRun = 1;
-int runToSide = 0;
+int leftOrRight = 0;
 int upOrDown = 0;
 
-void renderSomething(){
+void drawAxis(){
+    glColor3f(1, 0, 0);
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(100, 0, 0);
+    glEnd();
 
+    glColor3f(0, 1, 0);
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 100, 0);
+    glEnd();
+
+
+    glColor3f(0, 0, 1);
+    glBegin(GL_LINES);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0, 100);
+    glEnd();
+}
+
+void renderSomething(){
     glClearColor(0,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -16,12 +36,12 @@ void renderSomething(){
 
     glMatrixMode(GL_MODELVIEW);
 
-    if(firstRun){
+    drawAxis();
 
+    if(firstRun){
         glClearDepth(1.0f);
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LEQUAL);
-
         glRotated(90, 1, 0, 0);
         firstRun = 0;
     }
@@ -36,15 +56,14 @@ void renderSomething(){
     glRotated(10, 0, 0, 1);
     // PLOT EARTH
     glColor3d(0,1,1);
-    glutSolidSphere(5, 10, 10);
+    glutWireSphere(5, 10, 10);
     glTranslated(-80, 0, 0);
     
     glPopMatrix();
+    glRotated(leftOrRight, 0, 0, 1);
     // PLOT SUN
     glColor3d(1,1,0);
     glutSolidSphere(18, 10, 10);
-    glRotated(runToSide, 0, 0, 1);
-
 
     glFlush();
 }
@@ -52,21 +71,21 @@ void renderSomething(){
 void arrowsAction(int key, int x, int y){
     switch(key){
         case GLUT_KEY_RIGHT:
-            runToSide = 10;
+            leftOrRight = 10;
             glutPostRedisplay();
             break;
         case GLUT_KEY_LEFT:
-            runToSide = -10;
+            leftOrRight = -10;
             glutPostRedisplay();
             break;
         case GLUT_KEY_UP:
             upOrDown = 10;
             glutPostRedisplay();
-        break;
+            break;
         case GLUT_KEY_DOWN:
             upOrDown = -10;
             glutPostRedisplay();
-        break;
+            break;
     }
 }
 
