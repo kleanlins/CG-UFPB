c4fda7f44df695baac2e31a315f12ce6a1dde46a
Cleanderson Lins <cleandersonlins@gmail.com>
2019-04-03 17:34:23 -0300

    Finalized project

--- Exercicio 03-04/modeling.cpp
+++ Exercicio 03-04/modeling.cpp
@@ -1,5 +1,5 @@
 #include <GL/gl.h>
-#include <GL/glu.h>
+#include <GL/glut.h>
 #include <cstdlib>
 #include <vector>
 #include <string>
@@ -7,6 +7,11 @@
 #include <fstream>
 #include <cstdio>
 #include <iostream>
+
+int firstRun = 1;
+int leftOrRight = 0;
+int upOrDown = 0;
+int cube;
 
 struct coordinate{
     float x, y, z;
@@ -57,17 +62,20 @@ int loadObject(const char *filename){
     for(int i = 0; i < coord.size(); i++){
         if(coord[i]->c_str()[0] == '#')
             continue;
-        else if(coord[i]->c_str()[0] == 'v' && coord[i]->c_str()[1] == ' '){ // if it's just a vertex
+        // if it's just a vertex
+        else if(coord[i]->c_str()[0] == 'v' && coord[i]->c_str()[1] == ' '){  
             float temp_x, temp_y, temp_z;
             sscanf(coord[i]->c_str(), "v %f %f %f", &temp_x, &temp_y, &temp_z);
             vertex.push_back(new coordinate(temp_x, temp_y, temp_z));
-        }else if(coord[i]->c_str()[0] == 'v' && coord[i]->c_str()[1] == 'n'){ // if it's a normal
+        // if it's a normal
+        }else if(coord[i]->c_str()[0] == 'v' && coord[i]->c_str()[1] == 'n'){ 
             float temp_x, temp_y, temp_z;
             sscanf(coord[i]->c_str(), "vn %f %f %f", &temp_x, &temp_y, &temp_z);
             normals.push_back(new coordinate(temp_x, temp_y, temp_z));
-        }else if(coord[i]->c_str()[0] == 'f'){ // if it's a face
+        // if it's a face
+        }else if(coord[i]->c_str()[0] == 'f'){                                
             int a, b, c, d, e;
-            if(std::count(coord[i]->begin(), coord[i]->end(), " ") == 3){
+            if(std::count(coord[i]->begin(), coord[i]->end(), ' ') == 3){
                 sscanf(coord[i]->c_str(),"f %d//%d %d//%d %d//%d",&a,&b,&c,&b,&d,&b);
                 faces.push_back(new face(b,a,c,d));
             }else{
@@ -77,28 +85,142 @@ int loadObject(const char *filename){
         }
     }
 
-    //free memory
+    int listNumber;
+    listNumber = glGenLists(1);
+
+    glNewList(listNumber, GL_COMPILE);
+        for(int i = 0; i < faces.size(); i++){
+            if(faces[i]->four){
+                glBegin(GL_QUADS);
+                    glNormal3f(normals[faces[i]->facenum-1]->x,
+                    normals[faces[i]->facenum-1]->y, 
+                    normals[faces[i]->facenum-1]->z);
+
+                    // draw the face
+                    glVertex3f(vertex[faces[i]->faces[0]-1]->x,
+                    vertex[faces[i]->faces[0]-1]->y,
+                    vertex[faces[i]->faces[0]-1]->z);
+
+                    glVertex3f(vertex[faces[i]->faces[1]-1]->x,
+                    vertex[faces[i]->faces[1]-1]->y,
+                    vertex[faces[i]->faces[1]-1]->z);
+
+                    glVertex3f(vertex[faces[i]->faces[2]-1]->x,
+                    vertex[faces[i]->faces[2]-1]->y,
+                    vertex[faces[i]->faces[2]-1]->z);
+
+                    glVertex3f(vertex[faces[i]->faces[3]-1]->x,
+                    vertex[faces[i]->faces[3]-1]->y,
+                    vertex[faces[i]->faces[3]-1]->z);
+                glEnd();
+            }else{
+                glBegin(GL_TRIANGLES);
+                    glNormal3f(normals[faces[i]->facenum-1]->x,
+                    normals[faces[i]->facenum-1]->y, 
+                    normals[faces[i]->facenum-1]->z);
+
+                    // draw the face
+                    glVertex3f(vertex[faces[i]->faces[0]-1]->x,
+                    vertex[faces[i]->faces[0]-1]->y,
+                    vertex[faces[i]->faces[0]-1]->z);
+
+                    glVertex3f(vertex[faces[i]->faces[1]-1]->x,
+                    vertex[faces[i]->faces[1]-1]->y,
+                    vertex[faces[i]->faces[1]-1]->z);
+
+                    glVertex3f(vertex[faces[i]->faces[2]-1]->x,
+                    vertex[faces[i]->faces[2]-1]->y,
+                    vertex[faces[i]->faces[2]-1]->z);
+                glEnd();
+            }
+        }
+    glEndList();    
+    // free memory
     for(int i = 0; i < coord.size(); i++)
         delete coord[i];
+    for(int i = 0; i < normals.size(); i++)
+        delete normals[i];
+    for(int i = 0; i < faces.size(); i++)
+        delete faces[i];
+    for(int i = 0; i < vertex.size(); i++)
+        delete vertex[i];
 
+    return listNumber;
 }
 
 void init(){
-    glClearColor(1,1,1,1);
+    glClearColor(0.0,0.0,0.0,1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    gluPerspective(45, 640.0/480.0, 1.0, 500.0);
-
+    gluPerspective(20,1280.0/720.0,1.0,500.0);
+    // glOrtho(-100.0,100.0,-100.0,100.0, -100, 100);
+    // glFrustum(500, 500, 500, 500, 10, 100);
     glMatrixMode(GL_MODELVIEW);
     glEnable(GL_DEPTH_TEST);
 
-    //glClearDepth(1.0f);
-    //glDepthFunc(GL_LEQUAL);
+    cube = loadObject("cube.obj");
+
+    glEnable(GL_LIGHTING);  // enabling light
+    glEnable(GL_LIGHT0);
+
+    float col[]={1.0,1.0,1.0,1.0};  // light color
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,col);
 
 }
 
-void display(){
+void renderSomething(){
+    glClearColor(0,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
+
+    float pos[]={-1.0,1.0,-2.0,1.0}; // light position
+    glLightfv(GL_LIGHT0,GL_POSITION,pos);
+
+    glTranslatef(0.0,0.0,-10.0);
+    glRotated(leftOrRight, 0, 0, 1);
+
+    if(upOrDown){
+        glRotated(upOrDown, 0, 1, 0);
+        upOrDown = 0;
+    }
+
+    glCallList(cube); // finally draws the cube
+
+    glFlush();
+}
+
+void arrowsAction(int key, int x, int y){
+    switch(key){
+        case GLUT_KEY_RIGHT:
+            leftOrRight = 5;
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_LEFT:
+            leftOrRight = -5;
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_UP:
+            upOrDown = 10;
+            glutPostRedisplay();
+            break;
+        case GLUT_KEY_DOWN:
+            upOrDown = -10;
+            glutPostRedisplay();
+            break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Cleanderson Lins");
+
+    init();
+    glutDisplayFunc(renderSomething);
+    glutSpecialFunc(arrowsAction);
+
+    glutMainLoop();
 }
